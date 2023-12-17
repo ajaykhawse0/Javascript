d80b2a0fec682bd5271aba99f77c89a760c9d9ae
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2023-12-17 18:34:23 +0000

    Conversionoperation

---
+++ tushar/Conversionoperation.js
@@ -0,0 +1,22 @@
+// let score = true for boolean
+// let score = false
+
+
+// let score = "ajay" it can not be coverted in numbet because it is an string
+// i.e. NaN will be shown
+
+// console.log(score);
+
+// let score = "45abc";
+
+
+
+// console.log( typeof(score));
+
+// let valueinNumber = Number(score)
+
+// console.log(valueinNumber);
+
+// console.log(typeof valueinNumber);
+
+
