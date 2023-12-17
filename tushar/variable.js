76643c33f3e33bf03b24d450b6a4a2e55d13a666
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2023-12-17 17:38:34 +0000

    varaible

---
+++ tushar/variable.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail ="ajaykhawse2@gmail.com"
+var accountPassword = "23456"
+accountCity = "Bhopal"
+let accountState;
+
+// accountId = 2// not allowed becuse it is a const value//
+
+accountEmail= "tushar@google"
+accountPassword= "456"
+accountCity = "Multai"
+
+// console.log(accountEmail);//
+// console.log(accountPassword);//
+//* dont use var dont beacause of issue in blockspace and funtional space//
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
