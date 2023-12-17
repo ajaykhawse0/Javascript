2a28e8cb6a3e96a9db5c124effe9e8585cc231dd
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2023-12-17 18:06:19 +0000

    Datatype

---
+++ tushar/Datatypes.js
@@ -0,0 +1,35 @@
+ "use strict";// teart all JS code a newer version
+
+//  alert(3 + 2) in newer version it does not work 
+// for now as we are using NODE JS not browser so it will pop an error
+
+
+// console.log(3 + 2) // code redability should be high
+
+//  console.log("ajay")
+
+let name = "Ajay"
+let age = 17
+let isloggedin = false
+
+// string => ""
+// number => 2 to power 53
+// bigint
+// boolean => true or false
+// null => stand alone or type
+//  undefined => 
+// symbol is used in figma toolsif we want to use unquie things symbol is used
+// object
+
+// console.log (typeof undefined )
+
+
+
+// type of null is  object (INTERVIEW QUE.)
+// undefined is special type( if we write {type of undefined} it shows undefined)
+
+// console.log(typeof age)
+
+
+
+
