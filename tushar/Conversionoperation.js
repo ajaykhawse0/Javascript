53e11ef7b415e216fe523402b122e4b4ead960b4
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2023-12-18 22:39:02 +0530

    Update Conversionoperation.js

--- tushar/Conversionoperation.js
+++ tushar/Conversionoperation.js
@@ -24,3 +24,77 @@ console.log(typeof ValueinNumber);
 let ValueinNumber = String(ValueinNumber)
 console.log(typeof ValueinNumber);
 
+// let score = true for boolean
+// let score = false
+
+
+// let score = "ajay" it can not be coverted in numbet because it is an string
+// i.e. NaN will be shown
+
+// console.log(score);
+
+// let score = "45abc";
+
+
+
+// console.log( typeof(score));
+
+// let valueinNumber = Number(score)
+
+// console.log(valueinNumber);
+
+// console.log(typeof valueinNumber);
+
+let ValueinNumber = 44
+// console.log(typeof ValueinNumber);
+let ValueinString = String(ValueinNumber)
+// console.log(typeof ValueinString);
+
+//******** OPERATIONS   ********//
+
+let value = 3
+let negvalue = -value //(answer will be negative because we assign negvalue = {-value})
+
+// console.log(negvalue)
+
+/*** Arthematic Operations ***/
+// console.log( 2+2 );
+// console.log( 3-4 );
+// console.log( 3*3);
+// console.log(3**3);  // ** is used to identify the second term as power of first term//
+// console.log(4/2);
+// console.log(56%5); //** % this sign used to know reminders     **/
+
+let str1 = "Hello "
+let str2 = "Ajay"
+str3 = str1 + str2
+// console.log(str1 + str2);
+// console.log(str3);
+
+//console.log( 1 + 5*6.5 % 5); //bodmass rule will be used
+
+console.log("2" + "2" );// output will be 22 because dont add they come after each other
+
+console.log( "2" + 4 + 5);
+
+console.log(  4 + 5 + "2");// if the numberin string both are in same log them te number only adds if they are in front of the elements 
+
+console.log(  4 + "2" + 5);
+
+/**Boolean conversion **/
+
+console.log(+true);
+console.log(-true);
+console.log(+"");
+
+/**President operator postfix and prefix **/
+
+let gameCounter = 100
+gameCounter++// POST FIX
+++gameCounter// PRE FIX
+console.log(gameCounter);
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.//
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.//
+
