4670b588308224a73538c829cc171bad727a9e82
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2023-12-18 23:14:44 +0530

    Create Comparison.js

---
+++ tushar/Comparison.js
@@ -0,0 +1,28 @@
+console.log(2>3);
+console.log(3>2);
+console.log(2==1);
+console.log(2>=3);
+console.log(2>=1);
+console.log( 2 !== 1);
+
+//all answer will be in boolean value//
+
+
+/************************** ***************************/
+
+
+console.log( "3" > 4);
+console.log( "5" > 4 );// in this it will consider "inside element of this" as a number
+
+
+console.log( null > 0 );
+console.log( 0 > null ); 
+console.log( null == 0);//inthis it will consider null as zero 
+
+console.log( null >= 0);
+/** DOnt use this **/
+
+
+// === strtictly check and its datatype//
+console.log( "2" == 2);
+console.log( "2" === 2 );
