8a6ac3cd02c38cf1ae57f08ecd828b2af6c00e09
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2023-12-28 10:45:45 +0530

    Create Strings.js

---
+++ tushar/Strings.js
@@ -0,0 +1,29 @@
+const name = "Ajay"
+const alpha = 4
+// console.log(name + alpha); old way
+//new way
+console.log(`My name is ${name} and the alphabets in my name is ${alpha}` );
+// three types of string 
+////////////////////////////////////////////////////
+// let str1 = "This is a double quoted string.";
+
+// let str2 = 'This is a single quoted string.';
+
+
+
+
+// let str3 = `This is a template string.`;
+///////////////////////////////////////////////////
+
+const gameName = new String('Ajay Khawse') // String s cannot be small
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(new String)
+// console.log(gameName.charAt('4'))
+// console.log(gameName.charAt('3'))
+// console.log(gameName.indexOf('K'))
+const newString1 = " Ajay "
+console.log(newString1);
+console.log(newString1.trim());
+console.log(newString1.bold());
