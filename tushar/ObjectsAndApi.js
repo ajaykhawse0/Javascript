2f75a85af2766dbcbfa4592373f15957d425fa05
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2024-02-23 17:21:13 +0530

    Create ObjectsAndApi.js

---
+++ tushar/ObjectsAndApi.js
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const course = {
+    coursename : "JS in Hindi",
+    price : 999,
+ Teacher : "Hitesh",
+}
+const {Teacher}=course
+console.log(Teacher);
+
+////////Api//////////////
+
+// API= An API is a set of programming code that enables data transmission between one software product and another. It also contains the terms of this data exchange.
