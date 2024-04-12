38a36c4ef324200b1e3e843ec39f67d5e86cc8b9
ajaykhawse0 <154079458+ajaykhawse0@users.noreply.github.com>
2024-04-12 10:56:23 +0530

    Update DOM.js

--- tushar/DOM.js
+++ tushar/DOM.js
@@ -38,3 +38,48 @@ function lightmode(){
   element.className="light-mode";
   content.innerText = "Dark Mode Is OFF ";
   } 
+let h2 = document.querySelector("h2");
+
+console.dir(h2.innerText); 
+h2.innerText =h2.innerText + " From me"
+
+let div = document.querySelector(".box")
+// let divs = document.querySelectorAll(".box");
+console.log(div);
+// let i = 1 ;
+// for (div of divs) {
+//   div.innerText = `New value ${i}`;
+// i++;
+// }
+// divs[0].innerText = "Ajay";
+// divs[1].innerText = "Tushar";
+// divs[2].innerText = "Paras";   
+
+// let id = div.getAttribute('id')
+// console.log(id);
+
+// let name = div.getAttribute("class")
+// console.log(name);
+
+// let nae = div.setAttribute("id","Fox")
+// console.log(nae);
+
+// let j = document.querySelector("#j");
+// console.log(j);
+// j.style.background = prompt("Enter a color");
+// j.style.color ="white";
+// j.innerText="FF";
+//-----------------------------------------------------------------------------------------------
+let newbtn = document.createElement("Button")
+newbtn.innerText = "Click me"
+newbtn.style.background = "Red"
+newbtn.style.color = "White"
+document.querySelector("body").prepend(newbtn);
+
+//________-------------------------------------------------------------------
+
+let para = document.querySelector("p");
+para.classList.add("newpara");
+para.classList.add("newpara2");
+para.classList.remove("newpara2");
+
